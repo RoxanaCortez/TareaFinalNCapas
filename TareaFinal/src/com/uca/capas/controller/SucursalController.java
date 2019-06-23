76c2060f7b7d0142b18f0f3cb568beab047e485d
RoxanaCortez <00502211@uca.edu.sv>
2019-06-23 00:39:06 -0600

    implementacion de repositorio, capa de servicio y modificacion del controlador

--- TareaFinal/src/com/uca/capas/controller/SucursalController.java
+++ TareaFinal/src/com/uca/capas/controller/SucursalController.java
@@ -1,16 +1,29 @@
 package com.uca.capas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.uca.capas.domain.Sucursal;
+import com.uca.capas.service.SucursalService;
+
 @Controller
 public class SucursalController {
+	
+	@Autowired
+	private SucursalService service;
 	
 	@RequestMapping("/")
 	public ModelAndView home() {
 		ModelAndView mav = new ModelAndView("index");
-		mav.addObject("message", "Probando controlador");
+		
+		List<Sucursal> listSucursal = service.listAll();
+		
+		mav.addObject("listSucursal", listSucursal);
+		
 		return mav;
 	}
 
---
+++ TareaFinal/src/com/uca/capas/repositories/SucursalRepository.java
@@ -0,0 +1,9 @@
+package com.uca.capas.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.uca.capas.domain.Sucursal;
+
+public interface SucursalRepository extends CrudRepository<Sucursal, Integer>{
+	
+}
