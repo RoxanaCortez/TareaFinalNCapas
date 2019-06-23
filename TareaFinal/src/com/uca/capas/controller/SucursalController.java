74b83564abb66346c5ce8705e5538a3dc8553421
RoxanaCortez <00502211@uca.edu.sv>
2019-06-23 00:14:09 -0600

    implementacion del controlador

---
+++ TareaFinal/src/com/uca/capas/controller/SucursalController.java
@@ -0,0 +1,17 @@
+package com.uca.capas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SucursalController {
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("message", "Probando controlador");
+		return mav;
+	}
+
+}
